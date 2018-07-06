837c689c315ab4200d1410320e2e1b55368291d6
Phong Huỳnh <p7157568@gmail.com>
2018-07-07 00:39:53 +0700

    Initial commit

---
+++ app/src/main/java/example/test/phong/testbottomsheet/MainActivity.kt
@@ -0,0 +1,12 @@
+package example.test.phong.testbottomsheet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
